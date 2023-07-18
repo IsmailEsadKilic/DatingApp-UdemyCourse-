6bbd6c9e4e8be0e554b700dff287ac11066440a5
IsmailEsadKilic <teamfabulous21@gmail.com>
2023-07-18 16:24:22 +0300

    end of 3

---
+++ API/Program.cs
@@ -0,0 +1,22 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt => 
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200")); 
+
+app.MapControllers();
+
+app.Run();
